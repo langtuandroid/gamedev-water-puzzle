3c3f142f3cfa0284759a9b6479cbb5e92a1bcd32
Nikolay Shtanchenko <Nikolay071989@gmail.com>
2024-03-18 18:36:37 +0200

    ProdactionFix

--- Assets/water color sorting/Resources/Scripts/Managers/DeviceTypeChecker.cs
+++ Assets/water color sorting/Resources/Scripts/Managers/DeviceTypeChecker.cs
@@ -7,21 +7,18 @@ public static class DeviceTypeChecker
         
         float screenSizeInches =
             Mathf.Sqrt(Mathf.Pow(Screen.width / Screen.dpi, 2) + Mathf.Pow(Screen.height / Screen.dpi, 2));
-
-        Debug.Log("screenSizeInches = " + screenSizeInches);
+        
         if (screenSizeInches >= 7.0f) // Пороговое значение для определения планшета
         {
-            Debug.Log("Device Type: " + DeviceType.Tablet);
             return DeviceType.Tablet;
         }
         else
         {
-            Debug.Log("Device Type: " + DeviceType.Smartphone);
             return DeviceType.Smartphone;
         }
 
     }
-    
+
 }
 public enum DeviceType
 {
